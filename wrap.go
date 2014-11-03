d86233524294b3376d8358a058b34b28fb22ad13
djherbis <djherbis@gmail.com>
2014-11-03 18:26:27 -0500

    Cleaned up WrapReader/Writer

--- wrap.go
+++ wrap.go
@@ -12,68 +12,64 @@ type wrapper struct {
 	do     ByteFunc
 }
 
-type WrapWriter struct {
-	wrapper
-}
+type WrapWriter wrapper
 
 func NewWrapWriter(w io.WriteSeeker, wrapAt int64) *WrapWriter {
-	return &WrapWriter{wrapper{
+	return &WrapWriter{
 		seek:   w.Seek,
 		do:     w.Write,
 		wrapAt: wrapAt,
-	}}
+	}
 }
 
 func (w *WrapWriter) Write(p []byte) (n int, err error) {
-	n, err = w.Wrap(p, w.off)
+	n, err = Wrap(w.seek, w.do, p, w.off, w.wrapAt)
 	w.off = (w.off + int64(n)) % w.wrapAt
 	return n, err
 }
 
 func (w *WrapWriter) WriteAt(p []byte, off int64) (n int, err error) {
-	return w.Wrap(p, off)
+	return Wrap(w.seek, w.do, p, off, w.wrapAt)
 }
 
-type WrapReader struct {
-	wrapper
-}
+type WrapReader wrapper
 
 func NewWrapReader(r io.ReadSeeker, wrapAt int64) *WrapReader {
-	return &WrapReader{wrapper{
+	return &WrapReader{
 		seek:   r.Seek,
 		do:     r.Read,
 		wrapAt: wrapAt,
-	}}
+	}
 }
 
-func (w *WrapReader) Read(p []byte) (n int, err error) {
-	n, err = w.Wrap(p, w.off)
-	w.off = (w.off + int64(n)) % w.wrapAt
+func (r *WrapReader) Read(p []byte) (n int, err error) {
+	n, err = Wrap(r.seek, r.do, p, r.off, r.wrapAt)
+	r.off = (r.off + int64(n)) % r.wrapAt
 	return n, err
 }
 
-func (w *WrapReader) ReadAt(p []byte, off int64) (n int, err error) {
-	return w.Wrap(p, off)
+func (r *WrapReader) ReadAt(p []byte, off int64) (n int, err error) {
+	return Wrap(r.seek, r.do, p, off, r.wrapAt)
 }
 
 func (w *wrapper) Seek(off int64, rel int) (int64, error) {
 	return w.seek(off, rel)
 }
 
-func (w *wrapper) Wrap(p []byte, off int64) (n int, err error) {
+func Wrap(seek SeekFunc, do ByteFunc, p []byte, off int64, wrapAt int64) (n int, err error) {
 	var m int
 
 	for len(p) > 0 {
 
-		w.seek(off, 0)
+		seek(off, 0)
 
-		if off+len64(p) <= w.wrapAt {
-			if m, err = w.do(p); err != nil {
+		if off+len64(p) <= wrapAt {
+			if m, err = do(p); err != nil {
 				return n + m, err
 			}
 		} else {
-			space := w.wrapAt - off
-			if m, err = w.do(p[:space]); err != nil {
+			space := wrapAt - off
+			if m, err = do(p[:space]); err != nil {
 				return n + m, err
 			}
 		}
@@ -82,7 +78,7 @@ func (w *wrapper) Wrap(p []byte, off int64) (n int, err error) {
 		off += int64(m)
 		p = p[m:]
 
-		if off == w.wrapAt {
+		if off == wrapAt {
 			off = 0
 		}
 	}
