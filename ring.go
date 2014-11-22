192e29ebd97b95ec5fc8c0de2f7b608038d8bfab
djherbis <djherbis@gmail.com>
2014-11-21 20:26:13 -0500

    Fixed Ring buffer Full bug, its never Full since it always accepts writes

--- ring.go
+++ ring.go
@@ -26,7 +26,7 @@ func (buf *Ring) Cap() int64 {
 }
 
 func (buf *Ring) Read(p []byte) (n int, err error) {
-	if Full(buf) {
+	if buf.Len() == buf.Buffer.Cap() {
 		buf.WrapReader.Seek(buf.WrapWriter.Offset(), 0)
 	}
 	n, err = io.LimitReader(buf.WrapReader, buf.Len()).Read(p)
@@ -35,7 +35,7 @@ func (buf *Ring) Read(p []byte) (n int, err error) {
 }
 
 func (buf *Ring) ReadAt(p []byte, off int64) (n int, err error) {
-	if Full(buf) {
+	if buf.Len() == buf.Buffer.Cap() {
 		buf.WrapReader.Seek(buf.WrapWriter.Offset(), 0)
 	}
 	return buf.WrapReader.ReadAt(p, buf.WrapReader.Offset()+off)
@@ -44,8 +44,8 @@ func (buf *Ring) ReadAt(p []byte, off int64) (n int, err error) {
 func (buf *Ring) Write(p []byte) (n int, err error) {
 	n, err = buf.WrapWriter.Write(p)
 	buf.L += int64(n)
-	if buf.L > buf.Cap() {
-		buf.L = buf.Cap()
+	if buf.L > buf.Buffer.Cap() {
+		buf.L = buf.Buffer.Cap()
 	}
 	return n, err
 }
@@ -54,15 +54,15 @@ func (buf *Ring) WriteAt(p []byte, off int64) (n int, err error) {
 	n, err = buf.WrapWriter.WriteAt(p, off)
 	if off+int64(n) > buf.Len() {
 		buf.L = off + int64(n)
-		if buf.L > buf.Cap() {
-			buf.L = buf.Cap()
+		if buf.L > buf.Buffer.Cap() {
+			buf.L = buf.Buffer.Cap()
 		}
 	}
 	return n, err
 }
 
 func (buf *Ring) FFwd(off int) int {
-	if Full(buf) {
+	if buf.Len() == buf.Buffer.Cap() {
 		buf.WrapReader.Seek(buf.WrapWriter.Offset(), 0)
 	}
 	if int64(off) >= buf.Len() {
@@ -77,6 +77,6 @@ func (buf *Ring) FFwd(off int) int {
 func (buf *Ring) Reset() {
 	buf.Buffer.Reset()
 	buf.L = 0
-	buf.WrapReader = NewWrapReader(buf.Buffer, 0, buf.Cap())
-	buf.WrapWriter = NewWrapWriter(buf.Buffer, 0, buf.Cap())
+	buf.WrapReader = NewWrapReader(buf.Buffer, 0, buf.Buffer.Cap())
+	buf.WrapWriter = NewWrapWriter(buf.Buffer, 0, buf.Buffer.Cap())
 }
