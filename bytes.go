2936e4648710e0c113c4843da4ad619edb93be95
djherbis <djherbis@gmail.com>
2014-11-25 22:58:20 -0500

    Added Bytes, a ReadAt/WriteAt wrapper for []byte. And Wrapper, a ReadAt/WriteAt -> Buffer wrapper... Added the files, whoops.

---
+++ bytes.go
@@ -0,0 +1,19 @@
+package buffer
+
+type Bytes []byte
+
+func NewBytes(n int64) Buffer {
+	return NewWrapper(Bytes(make([]byte, n)), n)
+}
+
+// BUG(Dustin): off past end?
+func (b Bytes) WriteAt(p []byte, off int64) (n int, err error) {
+	return copy(b[off:], p), nil
+}
+
+// BUG(Dustin): off past end?
+func (b Bytes) ReadAt(p []byte, off int64) (n int, err error) {
+	return copy(p, b[off:]), nil
+}
+
+// BUG(Dustin): doesn't support gob yet.
